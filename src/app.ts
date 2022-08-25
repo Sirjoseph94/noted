0ff28a492e97751bfcb6632f582c5bb89339fd31
Dec <dec@GentleStorms-Air.local>
2022-08-26 00:15:14 +0100

    setting up prettier hooks

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ import express from "express";
 const app = express();
 const port = process.env.PORT;
 
-const a = 0
+const a = 0;
 app.get("/", (req, res) => {
-	res.send("Group 4 project!");
+  res.send("Group 4 project!");
 });
 
 app.listen(port, () => {
-	console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
