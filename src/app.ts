4fd1f408930683c85f175c18a39495e7629d07b7
Dec <dec@GentleStorms-Air.local>
2022-08-28 02:17:34 +0100

    some Express config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import api from "./routes";
 import { auth } from "express-openid-connect";
 
 dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 const port = process.env.PORT;
 const config = {
   authRequired: false,
@@ -14,7 +18,6 @@ const config = {
   clientID: "ewTuvQ3jkdTthAi3DgaOI4jwcE2SC3EI",
   issuerBaseURL: "https://dev-c4ds8a6y.us.auth0.com",
 };
-
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
