1c53431f5e83651dcaaaa0b3c2e43636afb00d85
Zemikime Christopher <zemikimechristopher@gmail.com>
2022-08-28 02:48:09 +0100

    Resolved conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { requiresAuth } from "express-openid-connect";
 
 const router = Router();
 
@@ -19,6 +20,10 @@ router.put("/note:id", (req, res) => {
 });
 router.delete("/note:id", (req, res) => {
   res.send("request to delete note");
+});
+
+router.get("/user", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 export default router;
