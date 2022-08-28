569359d5c4c564f9f8dc1f49c372d1e06ae285c0
Zemikime Christopher <zemikimechristopher@gmail.com>
2022-08-28 02:33:23 +0100

    Notes routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,23 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("api get endpoint");
+router.get("/notes", (req, res) => {
+  res.send("get all notes");
+});
+
+router.get("/note:id", (req, res) => {
+  res.send("get single note");
+});
+
+router.post("/note", (req, res) => {
+  res.send("request to create note");
+});
+
+router.put("/note:id", (req, res) => {
+  res.send("update note");
+});
+router.delete("/note:id", (req, res) => {
+  res.send("request to delete note");
 });
 
 export default router;
