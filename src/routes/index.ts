9a03437218a1e461ea1ccf5a76ac9da99e6fb4c7
Zemikime Christopher <zemikimechristopher@gmail.com>
2022-08-29 20:17:52 +0100

    conflict resolved

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,6 +14,24 @@ router.post("/signup", async (req, res) => {
     res.status(400).json({ Error: error });
   }
 });
+router.get("/notes", (req, res) => {
+  res.send("get all notes");
+});
+
+router.get("/note:id", (req, res) => {
+  res.send("get single note");
+});
+
+router.post("/note", (req, res) => {
+  res.send("request to create note");
+});
+
+router.put("/note:id", (req, res) => {
+  res.send("update note");
+});
+router.delete("/note:id", (req, res) => {
+  res.send("request to delete note");
+});
 
 router.post("/signin", async (req, res) => {
   try {
